15f2b5e3d4b14ba354fac9e588908dbcb1742063
mndzfx <firnandivanpersi@gmail.com>
2025-07-23 08:55:34 +0700

    update

--- index.php
+++ index.php
@@ -3,10 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="refresh" content="0;url=pangan.smkn2kraksaan.sch.id">
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+  $newURL = "http://pangan.smkn2kraksaan.sch.id"; // Ganti dengan URL tujuan
+  header("Location: " . $newURL);
+  exit();
+?>
 </body>
 </html>

02ec7febf6487ecd27f952932a527417dd578c62
mndzfx <firnandivanpersi@gmail.com>
2025-07-23 08:52:39 +0700

    update

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-<?php
-header("Location: http://pangan.smkn2kraksaan.sch.id");
-exit();
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="0;url=pangan.smkn2kraksaan.sch.id">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
